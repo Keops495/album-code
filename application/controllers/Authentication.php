6c1c253d3c866c700264add4c872900d50c8eb53
Rau7 <tokeralp@gmail.com>
2018-11-12 02:03:32 +0300

    session

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -1,28 +1,37 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Authentication extends CI_Controller {
+  public function index(){
+    
+    if(isset($this->session->userdata['user']['user_id'])){
+      //$data['subview'] = "subviews/dashboard";
+      //$data['title'] = "EIB Admin - Anasayfa";  
+     
+      header(base_url()."User/");
+    }
+    else{
+      $data['title'] = "BooksThatYouLove Admin - Giriş";
+      if($this->input->post()){
+        $this->load->model("login_model");
+        $admin = $this->Login_model->getAdmin($this->input->post());
+        if(empty($admin)){
+          $this->session->set_flashdata('error', 'Hatalı kullanıcı adı veya şifre girdiniz.');
+          header("Location: ".base_url()."Authentication");
+        }
+        else{
+          $this->session->set_userdata('admin',$admin);
+          header("Location: ".base_url());
+        }
+      }
+      else{
+        $this->load->view('layouts/login',$data);
+      }
+    }
+  }
 
-	public function index()
-	{
-		if($this->input->post()) {
-			$d = $this->input->post();
-			var_dump($d);
-			die();
-		    $this->load->model('Authentication_model');
-
-		    $data=$this->Authentication_model->get_pass($username);
-
-		    if($data == $id) {
-		    	$this->load->view('user_detail');
-		    } else {
-		    	$this->load->view('login');
-		    }
-		} else {
-			$this->load->view('login');
-		}
-
-
-		
-	}
+  public function out(){
+    $this->session->sess_destroy();
+    header("Location: ".base_url());
+  }
 }
+
