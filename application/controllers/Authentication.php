67af4cd50127ed3bb85e5f93f4d94c2560deb53b
yalimbilgin <yalimbilgin@gmail.com>
2018-11-12 13:02:59 +0300

    ->

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -32,7 +32,7 @@ class Authentication extends CI_Controller {
                   $date = new DateTime($e['photo_date']);
                   $date2 = new DateTime(date("Y/m/d"));
                   $temp = date_diff($date,$date2);
-                  if($temp['y']>0 OR $temp['m']>0 OR $temp['d'] >= $v['n_times']) {
+                  if($temp->y>0 OR $temp->m>0 OR $temp->d >= $v['n_times']) {
                     $this->User_model->delete_photo($e['photo_id']);
                   }
                 }  
