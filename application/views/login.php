402834ce4beffd2ba959be4db269280fcdbff450
Rau7 <tokeralp@gmail.com>
2018-11-12 10:53:44 +0300

    margin changes

--- application/views/login.php
+++ application/views/login.php
@@ -16,7 +16,7 @@
         <form action="https://keops-web1.herokuapp.com/Authentication/log" method='POST' enctype="multipart/form-data" style="text-align: center;">
           <input type="text" name="Username" class="form-control" placeholder="Enter username" aria-describedby="basic-addon1" required >
           <input type="password" name="Password" class="form-control" placeholder="Enter password" aria-describedby="basic-addon1" required >   
-          <input type="submit" name="Sepet" value="Login" class="button" style="padding: 3px 6px; font-size: 200px;">
+          <input type="submit" name="Sepet" value="Login" class="button" style="padding: 3px 6px; font-size: 20px;">
         </form>
       </div>
     </div>
