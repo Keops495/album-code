c1652541efc6cc999c36671081da2528d41b9646
yalimbilgin <yalimbilgin@gmail.com>
2018-11-23 17:31:35 +0300

    API change

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -7,5 +7,20 @@ class Api extends CI_Controller {
     echo json_encode($this->Data_model->get_all_users());
   }
 
+   public function photos(){
+    $this->load->model('Data_model');
+    echo json_encode($this->Data_model->get_all_photos());
+  }
+
+   public function likes(){
+    $this->load->model('Data_model');
+    echo json_encode($this->Data_model->get_all_likes());
+  }
+
+   public function albums(){
+    $this->load->model('Data_model');
+    echo json_encode($this->Data_model->get_all_albums());
+  }
+
 }
 
