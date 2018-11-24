142810fb02db0777c8fa0622d469a6f27e7d1bb6
Rau7 <tokeralp@gmail.com>
2018-11-24 20:27:46 +0300

    upload v13

--- application/views/add_photograph.php
+++ application/views/add_photograph.php
@@ -22,8 +22,12 @@
 
           <div class="row">
             <form action="https://keops-web1.herokuapp.com/User/create_photo/<?php echo $album_id; ?>" method='POST' enctype="multipart/form-data" style="text-align: center; margin-left: 36%;">
-              <input type="file" name="Url" class="form-control" placeholder="Upload PHOTO" aria-describedby="basic-addon1" required style="margin-left: 25%; text-align: center;">
+              <input type="text" name="Url" class="form-control" placeholder="Enter photo url" aria-describedby="basic-addon1" required style="margin-left: 25%; text-align: center;">
               <input type="text" name="Key" class="form-control" placeholder="Enter photo key" aria-describedby="basic-addon1" required style="margin-left: 25%; text-align: center;">
+              <div class='form-group'>
+                <label>Profile Photo</label>
+                <input type="file" style='margin-bottom: 10px;' name="register_profile_image" required>
+              </div>                        
               <input type="submit" name="Sepet" value="Add" class="button" style="padding: 3px 6px; font-size: 15px; margin-left: 50%;">
           </form>
           </div>
