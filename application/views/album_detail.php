09e491831db6e1d3c7f25a2f219012b7dda7dcb8
yalimbilgin <yalimbilgin@gmail.com>
2018-11-24 21:10:32 +0300

    style change

--- application/views/album_detail.php
+++ application/views/album_detail.php
@@ -69,7 +69,9 @@
                   <p class="card-text"><?php echo $photo["key"]["thekey"]; ?></p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
+                      <hr>
                       <a href="https://keops-web1.herokuapp.com/User/delete_photo/<?php echo $photo["photo_id"]; ?>"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                      <span style="margin-left: 5px;margin-right: 5px;">|</span>
                       <a href="https://keops-web1.herokuapp.com/Share/<?php echo $user_id; ?>/<?php echo $photo["photo_id"]; ?>"><i class="fa fa-paper-plane" aria-hidden="true"></i></a>
                     </div>
                     <small class="text-muted">Added at: <?php echo $photo["photo_date"]; ?></small>
