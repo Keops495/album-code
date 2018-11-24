af3a43ab922252f2f1b31bb748b8f7178b2f3af9
Rau7 <tokeralp@gmail.com>
2018-11-24 20:07:41 +0300

    upload v5

--- application/views/add_photograph.php
+++ application/views/add_photograph.php
@@ -6,17 +6,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
-
-    <script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-    <link rel="stylesheet" href="/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
-    <script>
-        $('.iframe-btn').fancybox({ 
-            'width'     : 900,
-            'height'    : 600,
-            'type'      : 'iframe',
-            'autoScale'     : false
-        });
-    </script>
 </head>
 <body>
 
@@ -35,16 +24,17 @@
             <form action="https://keops-web1.herokuapp.com/User/create_photo/<?php echo $album_id; ?>" method='POST' enctype="multipart/form-data" style="text-align: center; margin-left: 36%;">
               <input type="text" name="Url" class="form-control" placeholder="Enter photo url" aria-describedby="basic-addon1" required style="margin-left: 25%; text-align: center;">
               <input type="text" name="Key" class="form-control" placeholder="Enter photo key" aria-describedby="basic-addon1" required style="margin-left: 25%; text-align: center;">
-              <div class="input-group">
-              <div class="input-icon">
-                  <i class="fa fa-image fa-fw"></i>
-                  <input id="photo_filemanager"
-                      value="thePhoto"
-                      class="form-control" type="text"
-                      name="thePhot"
-                      placeholder="Upload Photo" required>
+              
+              <div class="file-field">
+                  <div class="btn btn-primary btn-sm float-left">
+                      <span>Choose file</span>
+                      <input type="file">
+                  </div>
+                  <div class="file-path-wrapper">
+                      <input class="file-path validate" type="text" placeholder="Upload your file">
+                  </div>
               </div>
-            </div>   
+   
               <input type="submit" name="Sepet" value="Add" class="button" style="padding: 3px 6px; font-size: 15px; margin-left: 50%;">
           </form>
           </div>
