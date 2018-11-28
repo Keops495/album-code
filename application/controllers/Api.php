c13f2fd32e0b55ee637ca253493ffe7e82a58e03
yalimbilgin <yalimbilgin@gmail.com>
2018-11-28 10:25:57 +0300

    API change

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -16,6 +16,19 @@ class Api extends CI_Controller {
     $this->load->model('Data_model');
     echo json_encode($this->Data_model->get_all_likes());
   }
+    public function all() {
+      $this->load->model('Data_model');
+      $data['photos'] = $this->Data_model->get_all_photosa();
+      for ($j=0; $j <count($data['photos']) ; $j++) { 
+        $data['photos'][$j]['likes'] = $this->Data_model->get_likes($data["photos"][$j]["photo_id"]);
+        $data['photos'][$j]["key"]=$this->User_model->get_key($data["photos"][$j]["photo_id"]);
+        for ($i=0; $i < count($data["likes"]) ; $i++) { 
+          $data['photos'][$j]["likes"][$i]['name'] =$this->Data_model->get_name($data['photos'][$j]['likes'][$i]['like_user_id'])['name'];
+          $data['photos'][$j]["likes"][$i]['surname'] =$this->Data_model->get_surname($data['photos'][$j]['likes'][$i]['like_user_id'])['surname'];
+        }
+      }
+      
+    }
 
    public function albums(){
     $this->load->model('Data_model');
