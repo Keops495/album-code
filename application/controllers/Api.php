559f6667bbe17ca80b187c4d218bd91409b785fa
yalimbilgin <yalimbilgin@gmail.com>
2018-11-28 11:28:07 +0300

    shorter

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -19,13 +19,13 @@ class Api extends CI_Controller {
     public function all() {
       $this->load->model('Data_model');
       $this->load->model('User_model');
-      $data['photos'] = $this->Data_model->get_all_photosa();
-      for ($j=0; $j <count($data['photos']) ; $j++) { 
-        $data['photos'][$j]['likes'] = $this->Data_model->get_likes($data["photos"][$j]["photo_id"]);
-        $data['photos'][$j]["key"]=$this->User_model->get_key($data["photos"][$j]["photo_id"]);
-        for ($i=0; $i < count($data['photos'][$j]["likes"]) ; $i++) { 
-          $data['photos'][$j]["likes"][$i]['name'] =$this->Data_model->get_name($data['photos'][$j]['likes'][$i]['like_user_id'])['name'];
-          $data['photos'][$j]["likes"][$i]['surname'] =$this->Data_model->get_surname($data['photos'][$j]['likes'][$i]['like_user_id'])['surname'];
+      $data = $this->Data_model->get_all_photosa();
+      for ($j=0; $j <count($data) ; $j++) { 
+        $data[$j]['likes'] = $this->Data_model->get_likes($data[$j]["photo_id"]);
+        $data[$j]["key"]=$this->User_model->get_key($data[$j]["photo_id"]);
+        for ($i=0; $i < count($data[$j]["likes"]) ; $i++) { 
+          $data[$j]["likes"][$i]['name'] =$this->Data_model->get_name($data[$j]['likes'][$i]['like_user_id'])['name'];
+          $data[$j]["likes"][$i]['surname'] =$this->Data_model->get_surname($data[$j]['likes'][$i]['like_user_id'])['surname'];
         }
       }
       echo json_encode($data);
