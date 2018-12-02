10f549141ed16acbcbf882a0fe94ab3cc9ebad66
yalimbilgin <yalimbilgin@gmail.com>
2018-12-02 23:33:45 +0300

    api all

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -69,5 +69,20 @@ class Api extends CI_Controller {
       $this->User_model->n_change($temp['user_id'],$temp['n']);
     }
 
+     public function all() {
+      $this->load->model('Data_model');
+      $this->load->model('User_model');
+      $data = $this->Data_model->get_all_photosa();
+      for ($j=0; $j <count($data) ; $j++) { 
+        $data[$j]['likes'] = $this->Data_model->get_likes($data[$j]["photo_id"]);
+        $data[$j]["key"]=$this->User_model->get_key($data[$j]["photo_id"]);
+        for ($i=0; $i < count($data[$j]["likes"]) ; $i++) { 
+          $data[$j]["likes"][$i]['name'] =$this->Data_model->get_name($data[$j]['likes'][$i]['like_user_id'])['name'];
+          $data[$j]["likes"][$i]['surname'] =$this->Data_model->get_surname($data[$j]['likes'][$i]['like_user_id'])['surname'];
+        }
+      echo json_encode($data);
+    }
+
+
 }
 
